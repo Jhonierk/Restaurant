a527fe37099226d8b7d798a3f8f4e8da5a49d6e0
jhonmendex <jhonmendex@gmail.com>
2021-09-30 15:28:33 -0500

    agregar storage como diccionario

--- js/function.js
+++ js/function.js
@@ -1,43 +1,52 @@
-$("#modal1").click(() => (window.location.href = '../pages/Mapa.html'))
-$("#modal2").click(() => (window.location.href = '../pages/Mapa.html'))
-$("#modal3").click(() => (window.location.href = '../pages/Mapa.html'))
-$("#modal4").click(() => (window.location.href = '../pages/Mapa.html'))
-$("#modal5").click(() => (window.location.href = '../pages/Mapa.html'))
-$("#modal6").click(() => (window.location.href = '../pages/Mapa.html'))
-
+$("#modal1").click(() => (window.location.href = "../pages/Mapa.html"));
+$("#modal2").click(() => (window.location.href = "../pages/Mapa.html"));
+$("#modal3").click(() => (window.location.href = "../pages/Mapa.html"));
+$("#modal4").click(() => (window.location.href = "../pages/Mapa.html"));
+$("#modal5").click(() => (window.location.href = "../pages/Mapa.html"));
+$("#modal6").click(() => (window.location.href = "../pages/Mapa.html"));
 
 function add(number) {
-    var value = document.getElementById(`quantity${number}`).value;
-    var sum = ++value;
-    document.getElementById(`quantity${number}`).value = sum;
+  var value = document.getElementById(`quantity${number}`).value;
+  var sum = ++value;
+  document.getElementById(`quantity${number}`).value = sum;
 }
 
-
 function subtract(number) {
-    var value = document.getElementById(`quantity${number}`).value;
-    var sum = --value;
-    if (sum < 0) {
-        document.getElementById(`quantity${number}`).value = 0;
-    } else {
-        document.getElementById(`quantity${number}`).value = sum;
-    }
+  var value = document.getElementById(`quantity${number}`).value;
+  var sum = --value;
+  if (sum < 0) {
+    document.getElementById(`quantity${number}`).value = 0;
+  } else {
+    document.getElementById(`quantity${number}`).value = sum;
+  }
 }
 
 function addcart(number) {
-    var img = document.getElementById(`image${number}`).src;
-    var image = img.slice(29);
-    var title = document.getElementById(`title${number}`).innerHTML;
-    var description = document.getElementById(`description${number}`).innerHTML;
-    var costo = document.getElementById(`cost${number}`).innerHTML;
-    var cost = parseInt(costo.slice(2));
-    var quantity = document.getElementById(`quantity${number}`).value;
-    if (quantity == 0) {
-        alert("Por favor elija la cantidad del plato a pedir");
-    } else {
+  var img = document.getElementById(`image${number}`).src;
+  var image = img.slice(29);
+  var title = document.getElementById(`title${number}`).innerHTML;
+  var description = document.getElementById(`description${number}`).innerHTML;
+  var costo = document.getElementById(`cost${number}`).innerHTML;
+  var cost = parseInt(costo.slice(2));
+  var quantity = document.getElementById(`quantity${number}`).value;
+  if (quantity == 0) {
+    alert("Por favor elija la cantidad del plato a pedir");
+  } else {
+    let pedido = [];
+
+    pedido.push({
+      imagen: image,
+      title: title,
+      costo: costo,
+      cantidad: quantity,
+    });
+
+    localStorage.setItem("pedido", pedido);
+    /*
         localStorage.setItem(`image${number}`, image);
         localStorage.setItem(`title${number}`, title);
         localStorage.setItem(`description${number}`, description);
         localStorage.setItem(`cost${number}`, cost);
-        localStorage.setItem(`quantity${number}`, quantity);
-    }
+        localStorage.setItem(`quantity${number}`, quantity); */
+  }
 }
