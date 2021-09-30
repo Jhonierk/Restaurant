9a2bb1204b4f1270ec38287dc09f1568cba1d53b
Juan Naranjo <juanknaranjo72@gmail.com>
2021-09-29 23:52:33 -0500

    Actualizacion pedidos js

--- js/function.js
+++ js/function.js
@@ -7,22 +7,35 @@ $("#modal6").click(() => (window.location.href = '../pages/Mapa.html'))
 
 
 function add(number) {
-        var value = document.getElementById(`input${number}`).value;
+        var value = document.getElementById(`quantity${number}`).value;
         var sum = ++value;
-        document.getElementById(`input${number}`).value = sum;
+        document.getElementById(`quantity${number}`).value = sum;
 }
 
 
 function subtract(number) {
-    var value = document.getElementById(`input${number}`).value;
+    var value = document.getElementById(`quantity${number}`).value;
     var sum = --value;
     if (sum < 0) {
-        document.getElementById(`input${number}`).value = 0;
+        document.getElementById(`quantity${number}`).value = 0;
     } else {
-        document.getElementById(`input${number}`).value = sum;
+        document.getElementById(`quantity${number}`).value = sum;
     }
 }
 
-function addcart(){
-
+function addcart(number){
+    var image = document.getElementById(`image${number}`).src;
+    var title = document.getElementById(`title${number}`).innerHTML;
+    var description = document.getElementById(`description${number}`).innerHTML;
+    var cost = document.getElementById(`cost${number}`).innerHTML;
+    var quantity = document.getElementById(`quantity${number}`).value;
+    if (quantity==0){
+        alert("Por favor elija la cantidad del plato a pedir");
+    }else{
+        localStorage.setItem(`image${number}`, image);
+        localStorage.setItem(`title${number}`, title);
+        localStorage.setItem(`description${number}`, description);
+        localStorage.setItem(`cost${number}`, cost);
+        localStorage.setItem(`quantity${number}`, quantity);
+    }
 }
