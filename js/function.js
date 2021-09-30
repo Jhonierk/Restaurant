cc6138161215d12c148e95bd060b8affeda220d2
Juan Naranjo <juanknaranjo72@gmail.com>
2021-09-29 20:45:53 -0500

    Actualizacion pedidos js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,24 @@
-$("#modal1").click(()=>(window.location.href='../pages/Mapa.html'))
-$("#modal2").click(()=>(window.location.href='../pages/Mapa.html'))
-$("#modal3").click(()=>(window.location.href='../pages/Mapa.html'))
-$("#modal4").click(()=>(window.location.href='../pages/Mapa.html'))
-$("#modal5").click(()=>(window.location.href='../pages/Mapa.html'))
-$("#modal6").click(()=>(window.location.href='../pages/Mapa.html'))
+$("#modal1").click(() => (window.location.href = '../pages/Mapa.html'))
+$("#modal2").click(() => (window.location.href = '../pages/Mapa.html'))
+$("#modal3").click(() => (window.location.href = '../pages/Mapa.html'))
+$("#modal4").click(() => (window.location.href = '../pages/Mapa.html'))
+$("#modal5").click(() => (window.location.href = '../pages/Mapa.html'))
+$("#modal6").click(() => (window.location.href = '../pages/Mapa.html'))
+
+
+function add(number) {
+        var value = document.getElementById(`input${number}`).value;
+        var sum = ++value;
+        document.getElementById(`input${number}`).value = sum;
+}
+
+
+function subtract(number) {
+    var value = document.getElementById(`input${number}`).value;
+    var sum = --value;
+    if (sum < 0) {
+        document.getElementById(`input${number}`).value = 0;
+    } else {
+        document.getElementById(`input${number}`).value = sum;
+    }
+}
